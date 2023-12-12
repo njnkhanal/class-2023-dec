0df969d4595df1b62fc4105036b1f26893077da3
Nirajan Khanal <njnkhanal02@gmail.com>
2023-12-12 11:34:21 +0545

    jquery dropdown

---
+++ Jquery Basic/dropdown.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('#drop-btn').click(function (e) { 
+        e.preventDefault();
+        $('#drop-menu').toggle();
+        // $('#drop-menu').hide();
+    });
+});
